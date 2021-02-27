5473f4521fdc75b2bff53b2d3ce62bd4a7565397
ImmortalityBH <ImmortalityBH@gmail.com>
2021-02-27 16:48:51 -0600

    add options for application settings configuration

---
+++ Fauna Engine/src/Utility/Options/Options.h
@@ -0,0 +1,35 @@
+#pragma once
+
+#include <string>
+
+namespace Fuana 
+{
+	// use auto
+	class Options
+	{
+		class Resolution 
+		{
+		public:
+			Resolution();
+			Resolution(const uint32_t width, const uint32_t height);
+		public:
+			uint32_t width;
+			uint32_t height;
+		};
+		class GfxConfig
+		{
+		public:
+		
+		private:
+		
+		};
+	public:
+		static bool LoadOptions(const std::string& config_file, Options& option_cfg);
+		
+		// obtain reference to each member
+		Resolution& GetRes();
+	private:
+		Resolution screen_res;
+	}
+	
+}
